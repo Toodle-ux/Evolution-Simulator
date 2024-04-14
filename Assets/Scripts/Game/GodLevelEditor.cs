a1e08bf216af7b7758f03c40804e2eb77370d304
Zhiyue Huang <zhiyuehuang@yeah.net>
2024-04-14 22:31:12 +0800

    V 0.2 Level Editor Sketch
    The God player now can change the terrain to sand by left clicking.
    (TODO: Creature's stats)

--- Assets/Scripts/Game/GodLevelEditor.cs
+++ Assets/Scripts/Game/GodLevelEditor.cs
@@ -7,23 +7,62 @@ namespace EvolutionSimulator
 {
 	public partial class GodLevelEditor : ViewController
 	{
+		public Grid grid;
+		
 		public Tilemap GodTerrain;
 		public Tilemap CreatureTerrain;
 		
 		public TileBase GrassTile;
-		
-		void Start()
+		public TileBase SandTile;
+
+		public EasyGrid<GridData> gridDatas;
+
+		public int GodXOffset = -11;
+		public int GodYOffset = -6;
+
+		public int CreatureXOffset = 1;
+		public int CreatureYOffset = -6;
+
+        public void Awake()
+        {
+            gridDatas = FindObjectOfType<GridController>().GodTerrain;
+        }
+
+        void Start()
 		{
-			var gridDatas = FindObjectOfType<GridController>().GodTerrain;
 			
+			// initate the grid data and the tilemaps
 			gridDatas.ForEach((x, y, _) =>
 			{
-				var godTilePos = new Vector3Int(x - 11, y - 6);
+				gridDatas[x, y] = new GridData();
+				
+				var godTilePos = new Vector3Int(x + GodXOffset, y + GodYOffset);
 				GodTerrain.SetTile(godTilePos, GrassTile);
 
-				var creatureTilePos = new Vector3Int(x + 1, y - 6);
+				var creatureTilePos = new Vector3Int(x + CreatureXOffset, y + CreatureYOffset);
 				CreatureTerrain.SetTile(creatureTilePos, GrassTile);
 			});
 		}
-	}
+
+        private void Update()
+        {
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+			Vector3Int cellPosition = grid.WorldToCell(mouseWorldPos);
+
+            Vector3Int gridDataPos = new Vector3Int(cellPosition.x - GodXOffset, cellPosition.y - GodYOffset);
+			Vector3Int creatureCellPosition = new Vector3Int(cellPosition.x - GodXOffset + CreatureXOffset, cellPosition.y - GodYOffset + CreatureYOffset);
+
+			// change the terrain to sand
+			if (Input.GetKeyDown(KeyCode.Mouse0))
+			{
+				if (cellPosition.x < -1 && cellPosition.x >= -11 && cellPosition.y < 6 && cellPosition.y >= -6) 
+				{
+					GodTerrain.SetTile(cellPosition, SandTile);
+					gridDatas[gridDataPos.x, gridDataPos.y].TerrainState = TerrainStates.Sand;
+
+					CreatureTerrain.SetTile(creatureCellPosition, SandTile);
+				}
+			}
+        }
+    }
 }
